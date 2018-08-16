f44fce50271a137971056d373053fd4ed1a5cb1c
Unknown <j.meenesh@gmail.com>
2018-08-16 18:48:31 +0530

    Minor changes in view and common listing page

---
+++ configuratior/src/app/components/feature/model/view.model.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-view-feature-model',
+    templateUrl: '../../common/table.view.html',
+    styleUrls: ['./model.component.scss']
+})
+
+
+export class ViewFeatureModelComponent implements OnInit {
+
+    page_main_title = 'Model Feature';
+    table_title = this.page_main_title;
+
+    add_route_link = '/feature/model';
+
+    table_head_foot = ['Name', 'Position', 'Office', 'Age', 'Start Date', 'Salary'];
+    
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+   
+   
+}
