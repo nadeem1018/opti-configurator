96f4ac658bc4e6e0fca42912f2efc9d1392f0021
Ashish Devade <adevade@optiproerp.com>
2018-09-06 16:44:40 +0530

    delete popup implementation in all views

--- configuratior/src/app/components/common/custom-dialogs/custom-dialogs.component.ts
+++ configuratior/src/app/components/common/custom-dialogs/custom-dialogs.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+
+import * as $ from 'jquery';
 
 
 @Component({
@@ -8,11 +10,42 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class CustomDialogsComponent implements OnInit {
   @Input() dialogParams:any;
+  @Output() userSelectionValue = new EventEmitter();
+  public message:any;
+
+  language = JSON.parse(sessionStorage.getItem('current_lang'));
   constructor() { }
 
   ngOnInit() {
-  console.log(this.dialogParams);
+  //console.log(this.dialogParams);
+
+    if(this.dialogParams.length > 0){
+      this.message = this.dialogParams[0].message;
+      this.open_confirmation_dialog(); 
+    }
+  }
+
+  //Events
+  onOKPress(){
+    this.userSelectionValue.emit(true);
+    this.close_confirmation_dialog();
+  }
+
+  onCancelPress(){
+    this.userSelectionValue.emit(false);
+    this.close_confirmation_dialog();
   }
 
 
+  //Core Function 
+  //To open the dialog
+  open_confirmation_dialog(){
+        $('#custom_dialog_box').modal("show");
+  }
+
+  //To close the dialog
+  close_confirmation_dialog(){
+      $('#custom_dialog_box').modal("hide");
+      this.userSelectionValue.emit("true")
+  }
 }
