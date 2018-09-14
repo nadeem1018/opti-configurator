4e8b13aea8f8051564b72786084210491ab91d3a
Ashish Devade <adevade@optiproerp.com>
2018-09-14 19:54:45 +0530

    some hardcodings from login removed and modelbom explode code implemented

--- configuratior/src/app/components/common/lookup/lookup.component.ts
+++ configuratior/src/app/components/common/lookup/lookup.component.ts
@@ -115,6 +115,10 @@ export class LookupComponent implements OnInit {
         this.showTreeView();
       }
 
+      if (this.lookupfor == "tree_view__model_bom_lookup"){
+        this.showModelBOMTreeView();
+      }
+
       if(this.lookupfor == "associated_BOM"){
         this.showAssociatedBOMs();
       }
@@ -395,6 +399,26 @@ export class LookupComponent implements OnInit {
       }
     }
 
+  }
+
+  showModelBOMTreeView(){
+    this.popup_title = this.language.explode;
+    this.showLoader = true;
+    this.LookupDataLoaded = false;
+    //this.tree_data_json = this.dummy_json();
+    this.showLoader = false;
+    this.LookupDataLoaded = true;
+    if(this.serviceData !== undefined){
+      if (this.serviceData.length > 0) {
+        this.tree_data_json =  this.dummy_json();
+        //this.tree_data_json =this.serviceData;
+      
+       // setTimeout(function(){
+          $("#tree_view").modal('show');
+        //}, 5000);
+      }
+    }
+    
   }
 
   // dummy_json(){
