e9648e1bb4b0455def4fe37976b31cb086d25024
Ashish Devade <adevade@optiproerp.com>
2018-12-13 13:58:30 +0530

    company name show in dropdown

--- configuratior/src/app/components/login/login.component.ts
+++ configuratior/src/app/components/login/login.component.ts
@@ -178,11 +178,10 @@ export class LoginComponent implements OnInit {
     //This will get the psURL
     this.auth.getPSURL().subscribe(
       data => {
-        debugger
         if (data != null) {
           this.psURL = data;
           //For code analysis remove in live enviorments.
-          //this.psURL = "http://localhost:9500";
+          this.psURL = "http://localhost:9500";
           //this.psURL = "http://172.16.6.140/OptiAdmin";
          //this.psURL = "http://172.16.6.122/OptiproAdmin";
           sessionStorage.setItem('psURL', this.psURL);
