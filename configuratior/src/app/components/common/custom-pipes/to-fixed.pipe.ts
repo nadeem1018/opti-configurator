83cf6b3a1bddaff5daf9669b4da5ec3b58f6590a
Unknown <j.meenesh@gmail.com>
2018-12-20 17:53:50 +0530

    pipe

--- configuratior/src/app/components/common/custom-pipes/to-fixed.pipe.ts
+++ configuratior/src/app/components/common/custom-pipes/to-fixed.pipe.ts
@@ -7,25 +7,16 @@ export class ToFixedPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
 
-    switch (args) {
-      case "3": {
-        return parseFloat(value).toFixed(3);
-        break;
-      }
-      case "2": {
-        return parseFloat(value).toFixed(2);
-        break;
-      }
-      case "4": {
+    if (args == "2" ){
+      return parseFloat(value).toFixed(2);
+    } else if (args == "3") {
+      return parseFloat(value).toFixed(3);
+    } else if (args == "4") {
         return parseFloat(value).toFixed(4);
-        break;
-      }
-      default: {
+    } else  {
         return parseFloat(value).toFixed(3);
-        break;
-      }
-
     }
+
 
   }
 
