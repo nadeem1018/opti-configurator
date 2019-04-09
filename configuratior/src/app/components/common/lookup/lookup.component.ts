41ed2c0cbd175e09eeb2f897dd52539ed794f511
Aryan2808 <arvyas@optiproerp.com>
2019-04-09 19:22:01 +0530

    associative BOM

--- configuratior/src/app/components/common/lookup/lookup.component.ts
+++ configuratior/src/app/components/common/lookup/lookup.component.ts
@@ -1896,14 +1896,14 @@ export class LookupComponent implements OnInit {
     this.table_head = [
 
       {
-        field: 'OPTM_DISPLAYNAME',
+        field: 'DisplayName',
         title: this.language.Model_ModelName,
         type: 'text',
         width: '100',
         attrType: 'text'
       },
       {
-        field: 'OPTM_FEATUREDESC',
+        field: 'Description',
         title: this.language.Model_ModelDesc,
         type: 'text',
         width: '100',
